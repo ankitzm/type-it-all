c8d7d9928c4c7d117f3435259ae99a7d43e8aced
ankitzm <ankit.singh.zm@gmail.com>
2021-08-04 23:21:59 +0530

    typing challenge card complete

--- src/Component/TypingChallengeContainer.js
+++ src/Component/TypingChallengeContainer.js
@@ -9,19 +9,24 @@ function TypingChallengeContainer({
     wpm,
     timeRemaining,
     timerStarted,
+    testInfo,
+    inputHandler
 }) {
     return (
         <div className="flex flex-col flex-grow">
             <div className="flex flex-row justify-evenly">
-                <p className="w-1/3"><ChallengeDetailCard cardName="WORDS" cardValue={words} /></p>
-                <p className="w-1/3"><ChallengeDetailCard cardName="CHARACTERS" cardValue={characters} /></p>
-                <p className="w-1/3"><ChallengeDetailCard cardName="WPM" cardValue={wpm} /></p>
+                <div className="w-1/3"><ChallengeDetailCard cardName="WORDS" cardValue={words} /></div>
+                <div className="w-1/3"><ChallengeDetailCard cardName="CHARACTERS" cardValue={characters} /></div>
+                <div className="w-1/3"><ChallengeDetailCard cardName="WPM" cardValue={wpm} /></div>
             </div>
 
             <TypingChallenge
                 selectedParagraph={selectedParagraph}
                 timeRemaining={timeRemaining}
-                timerStarted={timerStarted} />
+                timerStarted={timerStarted}
+                testInfo={testInfo}
+                inputHandler={inputHandler}
+            />
         </div>
     )
 }
