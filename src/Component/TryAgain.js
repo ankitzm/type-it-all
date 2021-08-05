9d82fe67b71edfffc5584cc434ad154c53c8a5dd
ankitzm <ankit.singh.zm@gmail.com>
2021-08-05 13:16:11 +0530

    unnesasary border removed

--- src/Component/TryAgain.js
+++ src/Component/TryAgain.js
@@ -4,19 +4,19 @@ function TryAgain({ words, characters, wpm, startAgain }) {
     const text = `Check out the React app to find your typing speed here https://ankitzm.github.io/type-it-all/, mine is ${wpm} wpm, what's yours. `;
 
     return (
-        <div className="text-2xl border-2 text-justify">
-            <div className="text-3xl mb-6 underline font-bold">Here's the Result</div>
+        <div className="text-2xl text-justify">
+            <div className="text-3xl mb-6 underline font-bold text-center">Here's the Result</div>
 
-            <div className="items-center border-2 m-4">
+            <div className="items-center m-4">
                 <p >Words &emsp;&emsp; : {words}</p>
                 <p>Characters : {characters}</p>
                 <p>Speed &emsp;&emsp; : {wpm} WPM</p>
             </div>
 
-            <div className="border-2 text-xl">
+            <div className="text-xl">
                 <button
                     onClick={() => startAgain()}
-                    className="bg-gray-800 hover:bg-black text-white font-semibold py-2 px-4 mx-2 border border-gray-400 rounded shadow"
+                    className="bg-gray-800 hover:bg-black text-white font-semibold py-2 px-4 mx-2 rounded shadow"
                 >
                     Retry
                 </button>
