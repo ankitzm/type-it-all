23df5e833d769ffebfedf63bd7b94991b92b3909
ankitzm <ankit.singh.zm@gmail.com>
2021-08-07 13:21:03 +0530

    fixed responsive text

--- src/Component/ChallengeDetailCard.js
+++ src/Component/ChallengeDetailCard.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function ChallengeDetailCard( {cardName, cardValue} ) {
     return (
-        <div className="flex flex-col justify-center items-center py-4 m-auto border hover:shadow-lg">
-            <div className="font-semibold">{cardName}</div>
-            <div className="font-bold text-4xl">{cardValue}</div>
+        <div className="flex flex-col justify-center items-center py-2 sm:py-4 m-auto border hover:shadow-lg">
+            <div className="font-semibold text-sm sm:text-base">{cardName}</div>
+            <div className="font-bold text-2xl sm:text-4xl">{cardValue}</div>
         </div>
     )
 }
