6248b7426e3e974abc41f52f2d22b8bb07b7d82f
ankitzm <ankit.singh.zm@gmail.com>
2021-08-07 11:56:02 +0530

    Github fork added to right-up corner

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Lightning from "../Assets/lightning.svg"
+import ForkMe from './ForkMe/ForkMe'
 
 function Nav() {
   return (
     <nav className="w-full text-5xl bg-gray-800 h-full">
+      <ForkMe
+        url="https://github.com/ankitzm/type-it-all"
+      />
       <p className="font-bangers my-4 inline-block float-left ml-4 text-yellow-300">
-      <img src={Lightning} alt="lightning" className="h-14 inline-block" />
-      TYPE IT ALL
+        <img src={Lightning} alt="lightning" className="h-14 inline-block" />
+        TYPE IT ALL
       </p>
     </nav>
   )
